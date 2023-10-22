45f658f66af3a756f7ea2bc705e5a4712073cfba
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-10-22 18:25:36 +0530

    Update travel.py

--- travel.py
+++ travel.py
@@ -1,4 +1,40 @@
-print("traveling to madurai")
+operand1 = float(sys.argv[2])
+operand2 = float(sys.argv[3])
+
+if operation == 'add':
+    result = add(operand1, operand2)
+elif operation == 'subtract':
+    result = subtract(operand1, operand2)
+elif operation == 'multiply':
+    result = multiply(operand1, operand2)
+elif operation == 'divide':
+    result = divide(operand1, operand2)
+else:
+    print("Invalid operation. Available operations: add, subtract, multiply, divide")
+    sys.exit(1)
+
+print(f"Result: {result}")
+#--_---------_-__--__-_-_-_-_
+
+
+def main():
+    try:
+        num_list = list(map(float, input("Enter a list of numbers separated by spaces: ").split()))
+        if not num_list:
+            print("List is empty. Please enter numbers.")
+            return
+
+        average = sum(num_list) / len(num_list)
+        above_average = [num for num in num_list if num > average]
+        below_average = [num for num in num_list if num < average]
+
+        print(f"Average: {average:.2f}")
+        print(f"Numbers above average: {above_average}")
+        print(f"Numbers below average: {below_average}")
+    except ValueError:
+        print("Invalid input. Please enter valid numbers.")
+
+if __name__ == "__main__":print("traveling to madurai")
 print("Traveling in train")
 print("Traveling aalong with my family")
 print("two days travel")
