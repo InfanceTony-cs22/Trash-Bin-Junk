510e3069fadc7dd62c36623830a7794530b86717
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-11 06:37:34 +0530

    Update travel.py

--- travel.py
+++ travel.py
@@ -1,6 +1,18 @@
 operand1 = float(sys.argv[2])
 operand2 = float(sys.argv[3])
 
+
+if operation == 'add':
+    if operation == 'add':
+    result = add(operand1, operand2)
+elif operation == 'subtract':
+    result = subtract(operand1, operand2)
+elif operation == 'multiply':
+    result = multiply(operand1, operand2)
+elif operation == 'divide':
+    result =
+    result = add(operand1, operand2)
+elif operation == 'subtract':if operation == 'add':
 if operation == 'add':
     if operation == 'add':
     result = add(operand1, operand2)
