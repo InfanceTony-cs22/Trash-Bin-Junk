8c349b94806bbf64da47ce70099cb809ebbdf41a
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-11 06:38:41 +0530

    Update travel.py

--- travel.py
+++ travel.py
@@ -6,6 +6,11 @@ if operation == 'add':
     if operation == 'add':
     result = add(operand1, operand2)
 elif operation == 'subtract':
+
+if operation == 'add':
+    if operation == 'add':
+    result = add(operand1, operand2)
+elif operation == 'subtract':
     result = subtract(operand1, operand2)
 elif operation == 'multiply':
     result = multiply(operand1, operand2)
