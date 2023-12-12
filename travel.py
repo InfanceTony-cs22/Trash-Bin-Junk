bd871d3ad85b6650d45ed4470bcedf892208916a
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-12-12 19:33:46 +0530

    Update travel.py

--- travel.py
+++ travel.py
@@ -1,4 +1,13 @@
 operand1 = float(sys.argv[2])
+
+
+
+
+if operation == 'add':
+    if operation == 'add':
+    result = add(operand1, operand2)
+elif operation == 'subtract':
+    result = subtract(operand1, operand2)
 operand2 = float(sys.argv[3])
 
     result = add(operand1, operand2)
